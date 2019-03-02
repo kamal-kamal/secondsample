e3da9212fff0b027e07b2d21fe49901a55a7f875
kamal saluja <saluja_kamal2000@yahoo.com>
2019-03-02 20:36:31 +1100

    adding files

--- components/second.spec.js
+++ components/second.spec.js
@@ -3,6 +3,6 @@ const getAll = require('./second');
 describe('all cases', () => {
   it('first test', () => {
     const result = getAll(6);
-    expect(result).toBe(7);
+    expect(result).toBe(9);
   })
 })
